373a6a4590dae40da6161a3096970deab6ab7b51
joshsaul1996 <joshsaul1996@gmail.com>
2018-11-29 18:15:33 +0000

    In Progress Blackjack

---
+++ src/exercise2/BlackJack.java
@@ -0,0 +1,25 @@
+package exercise2;
+
+
+public class BlackJack {
+
+	public static void main(String[] args) {
+		
+		
+		
+		int firstNum = 14;
+		int secondNum = 20;
+		int blackJack = 21;
+		
+	public static int blackJackGame{
+			if ((firstNum || secondNum) > blackJack) {
+				System.out.println("0");
+			}
+				
+		}
+		
+		
+		
+	}
+}
+
